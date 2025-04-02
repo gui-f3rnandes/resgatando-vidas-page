8b92371e2f1301bdf955eda6a5eeabb5db56fd39
Pedro Ferreira <leandroferreira.guedes123@gmail.com>
2025-04-02 15:35:53 -0300

    feat(TypeService): adicionando os card dos tipos de serviço.

--- src/components/TypeService.jsx
+++ src/components/TypeService.jsx
@@ -1,6 +1,32 @@
 function TypeService(){
     return(
-        <section></section>
+        <section className="flex flex-col justify-center items-center bg-[#D9D9D9] p-20">
+            <h1 className="text-3xl self-center font-bold -mb-15 ">Tipos de Serviços</h1>
+            <div className="flex flex-row w-[80vw] mt-[15vh] space-x-3">  
+                <div className="bg-[#F2F2F2] w-[30vw] rounded-md rounded-t-3xl ">
+                    <figure className="bg-[#C0C0C0] h-[30vh] rounded-t-3xl">
+
+                    </figure>
+                    <h2>Internação voluntária</h2>
+                    <h3>Consentimento do paciente: o indivíduo decide se internar, assinando uma declaração no momento da admissão.</h3>
+                </div>
+                <div className="bg-[#F2F2F2] w-[30vw] rounded-md rounded-t-3xl ">
+                    <figure className="bg-[#C0C0C0] h-[30vh] rounded-t-3xl">
+
+                    </figure>
+                    <h2>Internação voluntária</h2>
+                    <h3>Consentimento do paciente: o indivíduo decide se internar, assinando uma declaração no momento da admissão.</h3>
+                </div>
+                <div className="bg-[#F2F2F2] w-[30vw] rounded-md rounded-t-3xl ">
+                    <figure className="bg-[#C0C0C0] h-[30vh] rounded-t-3xl">
+
+                    </figure>
+                    <h2>Internação voluntária</h2>
+                    <h3>Consentimento do paciente: o indivíduo decide se internar, assinando uma declaração no momento da admissão.</h3>
+                </div>
+                
+            </div>
+        </section>
     )
 }
 
