d9f4f795a54daaee48a79a514059baba8df1727a
Pedro Ferreira <leandroferreira.guedes123@gmail.com>
2025-04-02 21:15:30 -0300

    style(TypeService): retomando a estilização dos cards.

--- src/components/TypeServiceComponents/TPSCards.jsx
+++ src/components/TypeServiceComponents/TPSCards.jsx
@@ -1,26 +1,19 @@
-import { Check } from "lucide-react"
-
-function TypeService(){
+function TPSCards({type,srcImage}){
     return(
-        <section className="flex flex-col justify-center items-center bg-[#D9D9D9] p-20">
-            <h1 className="text-3xl self-center font-bold -mb-15 ">Tipos de Serviços</h1>
-            <div className="flex flex-row w-[80vw] mt-[15vh] space-x-3">  
+        <div className="flex flex-row w-[80vw] mt-[15vh] space-x-3">  
                 <div className="bg-[#F2F2F2] w-[30vw] rounded-md rounded-t-3xl space-y-3 " >
                     <figure className="bg-[#C0C0C0] h-[40vh] rounded-t-3xl">
-                        <img  className="w-full h-full object-cover " src="*/" alt="imagem ilustrativa" />
+                        <img  className="w-full h-full object-cover " src={srcImage} alt="imagem ilustrativa" />
                     </figure>
                     <div className="px-3 space-y-3">
                         
-                        <h2 className="text-xl"><strong>Internação voluntária</strong></h2>
+                        <h2 className="text-xl"><strong>{type}</strong></h2>
                      
                         <div  className="flex flex-row p-2 space-x-3"> 
                         <Check   className="min-h-5 min-w-5"/>
                             <h3><strong>Consentimento do paciente:</strong> o indivíduo decide se internar, assinando uma declaração no momento da admissão. lore</h3>
                         </div>
-                        <div  className="flex flex-row p-2 space-x-3"> 
-                        <Check   className="min-h-5 min-w-5"/>
-                            <h3><strong>Consentimento do paciente:</strong> o indivíduo decide se internar, assinando uma declaração no momento da admissão. lore</h3>
-                        </div>
+    
             
                     </div>
                 </div>
@@ -40,8 +33,7 @@ function TypeService(){
                 </div>
                 
             </div>
-        </section>
     )
 }
 
-export default TypeService
+export default TPSCards
