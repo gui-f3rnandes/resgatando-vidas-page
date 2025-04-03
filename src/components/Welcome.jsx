dda6197f3f78c768da69b97b72e5aeda81a265c1
Guilherme Fernandes <gui.ff3rnand3s@gmail.com>
2025-04-02 21:34:32 -0300

    feat(navbar&welcome): estilização e mudança de cores dinâmica

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,13 +1,16 @@
 import BasicInfos from "./basicInfos/BasicInfos";
 
-function Welcome(){
-    return(
-        <section className="p-40 bg-[url()]" id="inicio">
-                <BasicInfos
-                  pequenoTitulo={"Seja bem-vindo"}
-                  titulo={"Resgatando Vidas"}
-                  paragrafo={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis iure alias placeat amet nam totam magnam similique cupiditate maiores, quis repudiandae laborum vitae pariatur minus veniam esse rerum. Accusantium, fuga!`}
-                />
+function Welcome() {
+    return (
+        <section
+            className="w-full h-[800px] bg-cover bg-center flex xl:items-center items-end justify-start text-center text-gray-100 px-2 sm:px-16 md:px-32 lg:px-64 xl:px-96 bg-[url('../src/assets/familia.jpg')]"
+            id="inicio"
+        >
+            <BasicInfos
+                pequenoTitulo="Seja bem-vindo"
+                titulo="Resgatando Vidas"
+                paragrafo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis iure alias placeat amet nam totam magnam similique cupiditate maiores, quis repudiandae laborum vitae pariatur minus veniam esse rerum. Accusantium, fuga!"
+            />
         </section>
     )
 }
--- src/components/basicInfos/BasicInfos.jsx
+++ src/components/basicInfos/BasicInfos.jsx
@@ -1,34 +1,35 @@
 function BasicInfos({ pequenoTitulo, titulo, paragrafo }) {
-  return (
-    <>
-      <div
-        className="flex flex-col items-center text-center gap-2.5 mb-10 px-4 
+    return (
+        <>
+            <div
+                className="flex flex-col items-center text-center gap-2.5 mb-10 px-4 
                    md:items-start md:text-left"
-      >
-        <h4 className="text-xl font-bold">{pequenoTitulo}</h4>
-        <h1 className="text-3xl font-bold">{titulo}</h1>
-        {/**
-         * Mudanças visando mobile para paragrafo:
-         * 
-         * ## `hyphens-auto` `break-words`:
-         * - `hyphens-auto`: Hifenização automática (certifique-se que seu HTML tem lang="pt-BR" declarado)
-         * - `break-words`: Força quebra em palavras longas quando necessário
-         * 
-         * ## `leading-snug` `tracking-tight`:
-         * - `leading-snug`: Reduz ligeiramente o espaçamento entre linhas
-         * - `tracking-tight`: Reduz o espaçamento entre letras para melhor compactação
-        */}
-        <p className="max-w-2xl w-full text-justify hyphens-auto break-words leading-snug tracking-tight">{paragrafo}</p>
-        <a
-          href="tel:+"
-          className="bg-gray-400 text-black p-2 rounded-2xl max-w-80 w-full 
-                 text-center mt-3 md:w-auto md:px-8"
-        >
-          Entre em contato
-        </a>
-      </div>
-    </>
-  );
+            >
+                <h4 className="text-xl font-bold">{pequenoTitulo}</h4>
+                <h1 className="text-3xl font-bold">{titulo}</h1>
+                {/**
+                 * Mudanças visando mobile para paragrafo:
+                 * 
+                 * ## `hyphens-auto` `break-words`:
+                 * - `hyphens-auto`: Hifenização automática (certifique-se que seu HTML tem lang="pt-BR" declarado)
+                 * - `break-words`: Força quebra em palavras longas quando necessário
+                 * 
+                 * ## `leading-snug` `tracking-tight`:
+                 * - `leading-snug`: Reduz ligeiramente o espaçamento entre linhas
+                 * - `tracking-tight`: Reduz o espaçamento entre letras para melhor compactação
+                */}
+                <p className="max-w-2xl w-full text-justify hyphens-auto break-words leading-snug tracking-tight">{paragrafo}</p>
+                <a
+                    href="tel:+"
+                    className="bg-[#8B5E3B] text-gray-200 p-3 rounded-xl w-full text-center mt-3 md:w-auto md:px-16 font-bold shadow-md hover:bg-[#5F7A49] transition-all duration-300 ease-in-out transform hover:scale-105"
+                >
+                    Entre em contato
+                </a>
+
+
+            </div>
+        </>
+    );
 }
 
 export default BasicInfos;
