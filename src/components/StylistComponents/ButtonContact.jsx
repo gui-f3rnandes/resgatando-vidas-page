87155dc15a689e84d3c40247aa78701029ff3c5b
Pedro Ferreira <leandroferreira.guedes123@gmail.com>
2025-04-04 19:03:50 -0300

    style(ButtonContact): adaptação do  hover color

--- src/components/StylistComponents/ButtonContact.jsx
+++ src/components/StylistComponents/ButtonContact.jsx
@@ -4,7 +4,7 @@ import { Children } from "react"
 
 function ButtonContact(props){
 
-    let Class="bg-[#8B5E3B] text-gray-200 p-3 hover:bg-[#5F7A49] transition-all duration-300 ease-in-out transform hover:scale-105 font-bold shadow-md"
+    let Class="bg-[#8B5E3B] text-gray-200 p-3 hover:bg-[#51683f] transition-all duration-300 ease-in-out transform hover:scale-105 font-bold shadow-md"
 
     return(
         <a 
